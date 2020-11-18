782d9ecdfdc6146984859b04560df34500ccfaea
Thomas Haagsma <t_haagsma1998@hotmail.com>
2020-11-18 17:04:48 +0100

    Added final test functionaties

--- Trigger technologieen test project/Assets/Scripts/ShowStateScr.cs
+++ Trigger technologieen test project/Assets/Scripts/ShowStateScr.cs
@@ -16,58 +16,52 @@ public class ShowStateScr : MonoBehaviour
     public Text arState;
     public Text platform;
     public Text stability;
+    public Text timer;
     private TrackableBehaviour vuforiaTrackable;
     private TrackingState arFoundationTrackable;
 
     public GameObject vuforiaGameObject;
     public GameObject arFoundationGameObject;
 
+    public GameObject imageTarget;
+    public GameObject objectTarget;
+
+    public GameObject arFoundationUiElements;
+    public GameObject vuforiaUiElements;
+
     // Start is called before the first frame update
     void Start()
     {
         arPlatform = autoDetectARPlatform();
-        changeARPlatform(arPlatform);
-
-        if (arPlatform == ARplatform.Vuforia)
-        {
-            vuforiaTrackable = FindObjectOfType<TrackableBehaviour>().GetComponent<TrackableBehaviour>();  
-        }
-
-        SetPlatformToText(arPlatform);
+        ChangeARPlatform(arPlatform);
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        if (arPlatform == ARplatform.ARFoundation)
-        {
-            SetARFoundationStateToText(arFoundationTrackable);
+    {
+        SetPlatformToText(arPlatform);
 
-            if (arFoundationTrackable == TrackingState.Tracking)
+        if (arPlatform == ARplatform.Vuforia)
+        {
+            SetVuforiaStateToText(vuforiaTrackable.CurrentStatus);//imageTarget.GetComponent<ImageTargetBehaviour>().CurrentStatus);
+
+            if (vuforiaTrackable.CurrentStatus == TrackableBehaviour.Status.TRACKED || vuforiaTrackable.CurrentStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
             {
                 StartCoroutine("CheckStability");
             }
-        }
-        else
-        {
-            SetVuforiaStateToText(vuforiaTrackable.CurrentStatus);
-
-            if (vuforiaTrackable.CurrentStatus == TrackableBehaviour.Status.TRACKED)
+            else if (vuforiaTrackable.CurrentStatus == TrackableBehaviour.Status.NO_POSE || vuforiaTrackable.CurrentStatus == TrackableBehaviour.Status.LIMITED)
             {
-                StartCoroutine("CheckStability");
-            }
-            else
-            {
+                StopCoroutine("CheckStability");
                 SetStabilityToText("Stable tracking not achieved");
                 stability.color = Color.red;
             }
         }
     }
 
-    private void SetARFoundationStateToText(TrackingState arFoundaitonState)
-    {
-        arState.text = "Tracking State: " + arFoundaitonState;
-    }
+    //private void SetARFoundationStateToText(TrackingState arFoundaitonState)
+    //{
+    //    arState.text = "Tracking State: " + arFoundaitonState;
+    //}
 
     private void SetVuforiaStateToText(TrackableBehaviour.Status vuforiaState)
     {
@@ -84,48 +78,58 @@ public class ShowStateScr : MonoBehaviour
         stability.color = Color.green;
     }
 
+    private void SetTimerValueToText(int timerValue)
+    {
+        timer.text = "Timer: " + timerValue + " seconds";
+    }
+
     private ARplatform autoDetectARPlatform()
     {
         return ARplatform.Vuforia;
         //return ARplatform.ARFoundation;
     }
 
-    public void changeARPlatform(ARplatform newARPlatform)
+    public void ChangeARPlatform(ARplatform newARPlatform)
     {
         arPlatform = newARPlatform;
 
         if (newARPlatform == ARplatform.Vuforia)
         {
-            disableARFaundation();
-            enableVuforia();
+            DisableARFaundation();
+            EnableVuforia();
         }
         else if (newARPlatform == ARplatform.ARFoundation)
         {
-            disableVuforia();
-            enableARFoundation();
+            DisableVuforia();
+            EnableARFoundation();
         }
     }
 
-    private void enableVuforia()
+    private void EnableVuforia()
     {
         VuforiaRuntime.Instance.InitVuforia();
         vuforiaGameObject.SetActive(true);
+        vuforiaTrackable = FindObjectOfType<TrackableBehaviour>().GetComponent<TrackableBehaviour>();
+        vuforiaUiElements.SetActive(true);
     }
 
-    private void disableVuforia()
+    private void DisableVuforia()
     {
         vuforiaGameObject.SetActive(false);
         VuforiaRuntime.Instance.Deinit();
+        vuforiaUiElements.SetActive(false);
     }
 
-    private void enableARFoundation()
+    private void EnableARFoundation()
     {
         arFoundationGameObject.SetActive(true);
+        arFoundationUiElements.SetActive(true);
     }
 
-    private void disableARFaundation()
+    private void DisableARFaundation()
     {
         arFoundationGameObject.SetActive(false);
+        arFoundationUiElements.SetActive(false);
     }
 
     private IEnumerator CheckStability()
@@ -134,4 +138,58 @@ public class ShowStateScr : MonoBehaviour
         SetStabilityToText("Stable tracking achieved");
     }
 
+    private IEnumerator Timer(int timerDuration)
+    {
+        int timerValue = 0;
+        bool timerActive = true;
+        while (timerActive)
+        {
+            yield return new WaitForSeconds(1f);
+            timerValue++;
+            SetTimerValueToText(timerValue);
+            if (timerValue == timerDuration)
+            {
+                timer.color = Color.green;
+                timerActive = false;
+            }
+        }
+
+        yield return new WaitForSeconds(5f);
+        timerValue = 0;
+        SetTimerValueToText(timerValue);
+        timer.color = Color.red;
+    }
+
+    public void ChangePlatformOnButtonPressed()
+    {
+        if (arPlatform == ARplatform.Vuforia)
+        {
+            ChangeARPlatform(ARplatform.ARFoundation);
+        }
+        else if (arPlatform == ARplatform.ARFoundation)
+        {
+            ChangeARPlatform(ARplatform.Vuforia);
+        }
+    }
+
+    public void ChangeTargetTypeOnButtonPressed()
+    {
+        if (imageTarget.activeSelf)
+        {
+            imageTarget.SetActive(false);
+            objectTarget.SetActive(true);
+            vuforiaTrackable = objectTarget.GetComponent<TrackableBehaviour>();
+        }
+        else if (objectTarget.activeSelf)
+        {
+            objectTarget.SetActive(false);
+            imageTarget.SetActive(true);
+            vuforiaTrackable = imageTarget.GetComponent<TrackableBehaviour>();
+        }
+    }
+
+    public void StartTimer()
+    {
+        StartCoroutine("Timer", 10);
+    }
 }
